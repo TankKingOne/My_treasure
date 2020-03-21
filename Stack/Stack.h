34e9f3688f6ea3afe2003934fb46cc056c71f571
TankKingOne <1174389158@qq.com>
2020-03-21 23:23:43 +0800

    stack模拟实现

---
+++ Stack/Stack.h
@@ -0,0 +1,24 @@
+#pragma once
+#include"Queue.h"
+
+typedef BTNode* STDataType;
+
+// 支持动态增长的栈
+//typedef int STDataType;
+typedef struct Stack
+{
+	STDataType* _a;
+	int _top; // 栈顶
+	int _capacity; // 容量
+}Stack;
+
+void StackInit(Stack* ps);
+void StackDestory(Stack* ps);
+
+void StackPush(Stack* ps, STDataType x);
+void StackPop(Stack* ps);
+STDataType StackTop(Stack* ps);
+int StackEmpty(Stack* ps);
+int StackSize(Stack* ps);
+
+void TestStack();
